bf55decb412423f937ea1be2c2dc256b0d0dffa6
Himanshu <hs7772043@gmail.com>
2024-02-02 17:58:32 +0530

    resolve header error

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -16,7 +16,7 @@ export default function Header() {
   const { theme } = useSelector((state) => state.theme);
   const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(() => {
+  useEffect(() => { 
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get('searchTerm');
     if (searchTermFromUrl) {
